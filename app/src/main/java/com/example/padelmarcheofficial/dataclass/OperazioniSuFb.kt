2572d870b6f58496a79c0930350d4b5a10b68daf
alexpugnaloni <s1092591@studenti.univpm.it>
2023-10-13 18:04:22 +0200

    continuo di home

--- app/src/main/java/com/example/padelmarcheofficial/dataclass/OperazioniSuFb.kt
+++ app/src/main/java/com/example/padelmarcheofficial/dataclass/OperazioniSuFb.kt
@@ -7,6 +7,7 @@ import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import java.io.ByteArrayOutputStream
 
