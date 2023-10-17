e494e61a4221aaf9097e0cb7f30a38257cc588fd
alexpugnaloni <s1092591@studenti.univpm.it>
2023-10-17 11:25:09 +0200

    inizio activity home pt2

--- app/src/main/java/com/example/padelmarcheofficial/AccediActivity.kt
+++ app/src/main/java/com/example/padelmarcheofficial/AccediActivity.kt
@@ -41,7 +41,7 @@ class AccediActivity : AppCompatActivity() {
             if (email.isNotEmpty() && password.isNotEmpty()){
                 firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener{
                     if (it.isSuccessful){
-                        val intent = Intent(this, MainActivity::class.java)
+                        val intent = Intent(this, HomeActivity::class.java)
                         startActivity(intent)
                     }else{
                         Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
