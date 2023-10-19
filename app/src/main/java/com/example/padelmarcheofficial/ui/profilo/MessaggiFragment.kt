3be5f60a2ef596821e13e049dec34d133d6d164b
alexpugnaloni <s1092591@studenti.univpm.it>
2023-10-19 16:38:07 +0200

    refactor generale

--- app/src/main/java/com/example/padelmarcheofficial/ui/profilo/MessaggiFragment.kt
+++ app/src/main/java/com/example/padelmarcheofficial/ui/profilo/MessaggiFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.padelmarcheofficial.databinding.FragmentProfiloBinding
+import com.example.padelmarcheofficial.databinding.FragmentMessaggiBinding
 
-class ProfiloFragment : Fragment() {
+class MessaggiFragment : Fragment() {
 
-    private var _binding: FragmentProfiloBinding? = null
+    private var _binding: FragmentMessaggiBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class ProfiloFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val profiloViewModel =
-            ViewModelProvider(this).get(ProfiloViewModel::class.java)
+        val messaggiViewModel =
+            ViewModelProvider(this).get(MessaggiViewModel::class.java)
 
-        _binding = FragmentProfiloBinding.inflate(inflater, container, false)
+        _binding = FragmentMessaggiBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textNotifications
-        profiloViewModel.text.observe(viewLifecycleOwner) {
+        messaggiViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
