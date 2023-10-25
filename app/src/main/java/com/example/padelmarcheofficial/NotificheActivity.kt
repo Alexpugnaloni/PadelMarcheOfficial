ced0e2d3d6e1a35b06edff5df89174b04e146f8e
alexpugnaloni <s1092591@studenti.univpm.it>
2023-10-25 17:07:27 +0200

    avvisi

--- app/src/main/java/com/example/padelmarcheofficial/NotificheActivity.kt
+++ app/src/main/java/com/example/padelmarcheofficial/NotificheActivity.kt
@@ -1,11 +1,28 @@
 package com.example.padelmarcheofficial
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.padelmarcheofficial.databinding.ActivityNotificheBinding
+
 
 class NotificheActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityNotificheBinding
+    private lateinit var btnBack: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_notifiche)
+        binding = ActivityNotificheBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        btnBack = findViewById<Button>(R.id.btnBack)
+        btnBack.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+
+
+        }
+
     }
 }
