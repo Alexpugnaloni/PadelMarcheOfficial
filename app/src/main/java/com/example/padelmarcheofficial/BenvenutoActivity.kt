b62ef9f294e6f0bfc9e736369339f7ead1e9467b
alexpugnaloni <s1092591@studenti.univpm.it>
2024-01-03 11:08:08 +0100

    codice documentato e ultima implementazione admin

--- app/src/main/java/com/example/padelmarcheofficial/BenvenutoActivity.kt
+++ app/src/main/java/com/example/padelmarcheofficial/BenvenutoActivity.kt
@@ -3,6 +3,7 @@ package com.example.padelmarcheofficial
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import com.example.padelmarcheofficial.admin.AdminActivity
 import com.example.padelmarcheofficial.dataclass.Account
 import com.example.padelmarcheofficial.dataclass.GestioneFirebase
@@ -20,15 +21,24 @@ class BenvenutoActivity : AppCompatActivity() {
 
     private var currentUser: FirebaseUser? = null
 
+    private val durata_splash_screen = 2000
+
     private var auth: FirebaseAuth = Firebase.auth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_benvenuto)
 
+        Handler().postDelayed(
+            {
+                prossimaActivity()
+            }, durata_splash_screen.toLong()
+        )
+    }
+
         /**
          * effettua un controllo se l'utente è già loggato, se non è loggato rimanda al login
          */
-
+    private fun prossimaActivity(){
         if (!checkUtenteisLoggato())
             startActivity(Intent(this, AccediActivity::class.java))
         else {
